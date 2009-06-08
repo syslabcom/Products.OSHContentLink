761da0a4be98a5d2aece97e3edae3bc6bd6df015
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2009-06-08 08:51:48 +0000

    added missing file interfaces.py

---
+++ Products/OSHContentLink/interfaces.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from zope.interface import Interface
+
+##code-section HEAD
+##/code-section HEAD
+
+class IOSH_Link(Interface):
+    """Marker interface for .OSH_Link.OSH_Link
+    """
+
+##code-section FOOT
+##/code-section FOOT
+
