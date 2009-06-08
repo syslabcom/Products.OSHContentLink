b41e8fa17ceea296f0291731e239c07e2ed24c50
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2009-06-08 09:10:16 +0000

    added missing files

---
+++ Products/OSHContentLink/handlers.py
@@ -0,0 +1,16 @@
+from DateTime import DateTime
+
+def handle_object_created(obj, event):
+    """ We can safely set the publication_date to the current date without
+    condition on creation. If the users manually sets a fate, it will take precendence."""
+    now = DateTime()
+    obj.setPublication_date(now)
+
+
+def handle_workflow_action(obj, event):
+    """ If an OSH_Links gets published, its effective date will be set to the current date """
+    if event.action=='publish':
+        now = DateTime()
+        obj.setEffectiveDate(now)
+        obj.reindexObject()
+
