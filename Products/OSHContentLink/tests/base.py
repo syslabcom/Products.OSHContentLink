964f42a770111cb19f10685e17a89c86875227df
gerken <gerken@97d050b9-b799-4550-a901-4f04598d4601>
2009-06-16 15:36:27 +0000

    Added Test Layer

--- Products/OSHContentLink/tests/base.py
+++ Products/OSHContentLink/tests/base.py
@@ -10,20 +10,31 @@ slows down test runner startup.
 from Testing import ZopeTestCase as ztc
 from Products.PloneTestCase import PloneTestCase as ptc
 
-ztc.installProduct('OSHContentLink')
-ptc.setupPloneSite(products=['OSHContentLink'])
+from Products.Five import fiveconfigure, zcml
+from Products.PloneTestCase import layer
+
+SiteLayer = layer.PloneSite
+
+class OSHContentLinkLayer(SiteLayer):
+    @classmethod
+    def setUp(cls):
+        ztc.installProduct('OSHContentLink')
+        ptc.setupPloneSite(products=['OSHContentLink'])
+        SiteLayer.setUp()
 
 class TestCase(ptc.PloneTestCase):
     """We use this base class for all the tests in this package. If
     necessary, we can put common utility or setup code in here. This
     applies to unit test cases.
     """
+    layer = OSHContentLinkLayer
 
 class FunctionalTestCase(ptc.FunctionalTestCase):
     """We use this class for functional integration tests that use
     doctest syntax. Again, we can put basic common utility or setup
     code in here.
     """
+    layer = OSHContentLinkLayer
 
     class Session(dict):
         def set(self, key, value):
