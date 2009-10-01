26aa58c2b3c6f1e761db8b05aa2bf04d49602899
gerken <gerken@97d050b9-b799-4550-a901-4f04598d4601>
2009-10-01 22:34:30 +0000

    Now we set the date correctly

--- Products/OSHContentLink/handlers.py
+++ Products/OSHContentLink/handlers.py
@@ -1,17 +1,23 @@
 from DateTime import DateTime
 
 def handle_object_created(obj, event):
-    """ We can safely set the publication_date to the current date without
-    condition on creation. If the users manually sets a fate, it will take precendence."""
+    """
+    If the user did not define a publication date, we set it to now
+    """
     if(not obj.Schema()['publication_date'].get(obj)):
         now = DateTime()
         obj.setPublication_date(now)
 
 
 def handle_workflow_action(obj, event):
-    """ If an OSH_Links gets published, its effective date will be set to the current date """
+    """ If an OSH_Links gets published, its effective date will be set to the current date.
+        If no Publication date was set, we set this too
+    """
     if event.action=='publish':
         now = DateTime()
         obj.setEffectiveDate(now)
         obj.reindexObject()
+        if(not obj.Schema()['publication_date'].get(obj)):
+            now = DateTime()
+            obj.setPublication_date(now)
 
