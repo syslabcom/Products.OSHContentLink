16140455e5f1d5f6cd9fe936dd7c7cd4b6b1c53f
gerken <gerken@97d050b9-b799-4550-a901-4f04598d4601>
2009-11-04 17:24:35 +0000

    merged

--- Products/OSHContentLink/handlers.py
+++ Products/OSHContentLink/handlers.py
@@ -4,10 +4,11 @@ def handle_object_created(obj, event):
     """
     If the user did not define a publication date, we set it to now
     """
-    if(not obj.Schema()['publication_date'].get(obj)):
-        now = DateTime()
-        obj.setPublication_date(now)
-
+    pass
+    # Previously we set a publication date here. We do that now
+    # below. I will not remove the event handler here for now, just
+    # the code. In case somebody complains about something.
+    # If you see this after summer 2010, get rid of the handler!
 
 def handle_workflow_action(obj, event):
     """ If an OSH_Links gets published, its effective date will be set to the current date.
